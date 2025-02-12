4c0307b0f4fafa0392a338b483a533c48d8b6782
Gehad10999 <ggehad894@gmail.com>
2025-02-12 20:15:02 +0200

    fixes

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.json());
 require('dotenv').config();
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: 'http://localhost:5000'
+    origin: 'http://localhost:3000'
 }));
 
 app.use(morgan('combined'));
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const app = require('./app');
-const PORT = 5000 ;
+const PORT = 3000 ;
 const server = http.createServer(app);
 // const socketIo = require('socket.io');
 const { pool , database}= require('./model/configration');
